5112c7e43bf028aeaba88840ab15fd95ac7f2a62
vikramsagar27121995 <81048755+vikramsagar27121995@users.noreply.github.com>
2021-03-20 20:58:25 +0000

    Add files via upload

---
+++ database/pds_db.sql
@@ -0,0 +1,238 @@
+-- phpMyAdmin SQL Dump
+-- version 5.0.3
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 18, 2021 at 11:24 PM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `pds_db`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `branches`
+--
+
+CREATE TABLE `branches` (
+  `id` int(30) NOT NULL,
+  `branch_code` varchar(50) NOT NULL,
+  `street` text NOT NULL,
+  `city` text NOT NULL,
+  `state` text NOT NULL,
+  `zip_code` varchar(50) NOT NULL,
+  `country` text NOT NULL,
+  `contact` varchar(100) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `branches`
+--
+
+INSERT INTO `branches` (`id`, `branch_code`, `street`, `city`, `state`, `zip_code`, `country`, `contact`, `date_created`) VALUES
+(5, 'ypjaPuMwBJeZtnx', 'High Street', 'London', 'London', 'IG7 1LR', 'United Kingdom', '0441234567890', '2021-03-14 12:50:38'),
+(6, 'SRCIJmcLwYPT5Fz', '128 Roden Street', 'Manchester', '', 'M1 1LW', 'United Kingdom', '0112334754', '2021-03-18 22:18:51');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parcels`
+--
+
+CREATE TABLE `parcels` (
+  `id` int(30) NOT NULL,
+  `reference_number` varchar(100) NOT NULL,
+  `sender_name` text NOT NULL,
+  `sender_address` text NOT NULL,
+  `sender_contact` text NOT NULL,
+  `recipient_name` text NOT NULL,
+  `recipient_address` text NOT NULL,
+  `recipient_contact` text NOT NULL,
+  `type` int(1) NOT NULL COMMENT '1 = Deliver, 2=Pickup',
+  `from_branch_id` varchar(30) NOT NULL,
+  `to_branch_id` varchar(30) NOT NULL,
+  `weight` varchar(100) NOT NULL,
+  `height` varchar(100) NOT NULL,
+  `width` varchar(100) NOT NULL,
+  `length` varchar(100) NOT NULL,
+  `price` float NOT NULL,
+  `status` int(2) NOT NULL DEFAULT 0,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `parcels`
+--
+
+INSERT INTO `parcels` (`id`, `reference_number`, `sender_name`, `sender_address`, `sender_contact`, `recipient_name`, `recipient_address`, `recipient_contact`, `type`, `from_branch_id`, `to_branch_id`, `weight`, `height`, `width`, `length`, `price`, `status`, `date_created`) VALUES
+(1, '201406231415', 'John Smith', 'Sample', '+123456', 'Claire Blake', 'Sample', 'Sample', 1, '1', '0', '30kg', '12in', '12in', '15in', 2500, 7, '2020-11-26 16:15:46'),
+(6, '505604168988', 'John Smith', 'Sample', '+123456', 'Sample', 'Sample', '+12345', 1, '1', '0', '23kg', '12in', '12in', '15in', 2500, 1, '2020-11-27 14:06:42'),
+(7, '167875472639', 'John Beiler', '389 MitReal Road, London SW1 3KI', '07448833922', 'Roger Frank', '9 Granville St Birmingham  B1 2LH', '07245127384', 1, '5', '', '10', '115', '6', '4', 120, 0, '2021-03-14 19:22:29'),
+(8, '867100418575', 'test', 'test', 'test', 'test', 'test', 'teste', 1, '5', '', '1', '29', '3', '39', 19, 0, '2021-03-18 22:14:03'),
+(9, '787347321125', 'yet', 'test', '075566443', 'test', 'test', '78754709', 2, '5', '5', '12', '20', '10', '20', 120, 0, '2021-03-18 22:22:59');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `parcel_tracks`
+--
+
+CREATE TABLE `parcel_tracks` (
+  `id` int(30) NOT NULL,
+  `parcel_id` int(30) NOT NULL,
+  `status` int(2) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `parcel_tracks`
+--
+
+INSERT INTO `parcel_tracks` (`id`, `parcel_id`, `status`, `date_created`) VALUES
+(1, 2, 1, '2020-11-27 09:53:27'),
+(2, 3, 1, '2020-11-27 09:55:17'),
+(3, 1, 1, '2020-11-27 10:28:01'),
+(4, 1, 2, '2020-11-27 10:28:10'),
+(5, 1, 3, '2020-11-27 10:28:16'),
+(6, 1, 4, '2020-11-27 11:05:03'),
+(7, 1, 5, '2020-11-27 11:05:17'),
+(8, 1, 3, '2020-11-27 11:05:26'),
+(9, 3, 2, '2020-11-27 11:05:41'),
+(10, 6, 1, '2020-11-27 14:06:57');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `system_settings`
+--
+
+CREATE TABLE `system_settings` (
+  `id` int(30) NOT NULL,
+  `name` text NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `contact` varchar(20) NOT NULL,
+  `address` text NOT NULL,
+  `cover_img` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `system_settings`
+--
+
+INSERT INTO `system_settings` (`id`, `name`, `email`, `contact`, `address`, `cover_img`) VALUES
+(1, 'Parcel Delivery System', 'info@pds.com', '+44 202 390 1345', '48 Liverpool Street London EC2M 7PP', '');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(30) NOT NULL,
+  `firstname` varchar(200) NOT NULL,
+  `lastname` varchar(200) NOT NULL,
+  `email` varchar(200) NOT NULL,
+  `password` text NOT NULL,
+  `type` tinyint(1) NOT NULL DEFAULT 2 COMMENT '1 = admin, 2 = staff',
+  `branch_id` int(30) NOT NULL,
+  `date_created` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- Dumping data for table `users`
+--
+
+INSERT INTO `users` (`id`, `firstname`, `lastname`, `email`, `password`, `type`, `branch_id`, `date_created`) VALUES
+(1, 'Administrator', '', 'admin@admin.com', '0192023a7bbd73250516f069df18b500', 1, 0, '2020-11-26 10:57:04'),
+(2, 'John', 'Smith', 'jsmith@sample.com', '1254737c076cf867dc53d60a0364f38e', 2, 1, '2020-11-26 11:52:04'),
+(3, 'George', 'Wilson', 'gwilson@sample.com', 'd40242fb23c45206fadee4e2418f274f', 2, 4, '2020-11-27 13:32:12'),
+(4, 'Test', 'B', 'test@test.com', 'ceb6c970658f31504a901b89dcd3e461', 2, 5, '2021-03-14 17:56:59'),
+(5, 'User1', 'Man', 'u1man@test.com', 'cc03e747a6afbbcbf8be7668acfebee5', 2, 6, '2021-03-18 22:21:41');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `branches`
+--
+ALTER TABLE `branches`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `parcels`
+--
+ALTER TABLE `parcels`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `parcel_tracks`
+--
+ALTER TABLE `parcel_tracks`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `branches`
+--
+ALTER TABLE `branches`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
+
+--
+-- AUTO_INCREMENT for table `parcels`
+--
+ALTER TABLE `parcels`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `parcel_tracks`
+--
+ALTER TABLE `parcel_tracks`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
+
+--
+-- AUTO_INCREMENT for table `system_settings`
+--
+ALTER TABLE `system_settings`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(30) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
